13afa2884aa0db027c4aedd5dda69d7b88bdbfc4
helena cao <81157365+manqing-cs@users.noreply.github.com>
2025-03-14 11:04:04 -0400

    adjust for json

--- ipfs.py
+++ ipfs.py
@@ -12,7 +12,7 @@ def pin_to_ipfs(data):
 	url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
 
 	response = requests.request("POST", url, json=data, headers=headers)
-	cid = response["IpfsHash"]
+	cid = response.json()["IpfsHash"]
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
@@ -22,6 +22,6 @@ def get_from_ipfs(cid,content_type="json"):
 	headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2MGU5N2EyMS1iMDBmLTQyZGYtODhmOS1jZTEwMTEzODVmYmEiLCJlbWFpbCI6Im1hbnFpbmdjYW9AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImI3YTAyNjE2ODM5NjkyMWNlNmM4Iiwic2NvcGVkS2V5U2VjcmV0IjoiNTY3ZmNmMWJkMmVkNWQ4ODNjZDkyMTQ0MjIxMWFkZDQwMjY5YTYyNGIzZGRhOTM3NzU3YjY0YjM4ZWExMzQ5NCIsImV4cCI6MTc3MzQ4MDc5MH0.nvZHj2jGH4D6VnkQpMZqKNtMCWSTvRRx1PaseO1EVgU"}
 	response = requests.request("GET", url, headers=headers)
 
-	data = response.text
+	data = response.json()
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 	return data
