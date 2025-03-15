58cd7008d114abd07a277a82551449747cc538d5
helena cao <81157365+manqing-cs@users.noreply.github.com>
2025-03-15 00:51:48 -0400

    get content

--- ipfs.py
+++ ipfs.py
@@ -18,7 +18,8 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
-	url = "https://gateway.pinata.cloud/ipfs/{cid}"
+	url =  "https://api.pinata.cloud/v3/files/private/{cid}"
+
 	headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2MGU5N2EyMS1iMDBmLTQyZGYtODhmOS1jZTEwMTEzODVmYmEiLCJlbWFpbCI6Im1hbnFpbmdjYW9AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImI3YTAyNjE2ODM5NjkyMWNlNmM4Iiwic2NvcGVkS2V5U2VjcmV0IjoiNTY3ZmNmMWJkMmVkNWQ4ODNjZDkyMTQ0MjIxMWFkZDQwMjY5YTYyNGIzZGRhOTM3NzU3YjY0YjM4ZWExMzQ5NCIsImV4cCI6MTc3MzQ4MDc5MH0.nvZHj2jGH4D6VnkQpMZqKNtMCWSTvRRx1PaseO1EVgU"}
 
 	response = requests.request("GET", url, headers=headers)
